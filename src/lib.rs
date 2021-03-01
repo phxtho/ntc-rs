85e19d7225f45a56534ea818ffd8a3e7cb2537af
phxtho <phathopukwana@gmail.com>
2021-03-01 21:58:14 +0200

    swtich to vecs as you  can't return arrays to js

--- src/lib.rs
+++ src/lib.rs
@@ -14,11 +14,12 @@ extern {
     fn alert(s: &str);
 }
 
-fn hex_color_to_rgb (hex_colour: &str) -> [u32;3] {
+#[wasm_bindgen]
+pub fn hex_color_to_rgb (hex_colour: &str) -> Vec<u32> {
     let r = hex_to_u32(&hex_colour[1..3]);
     let g = hex_to_u32(&hex_colour[3..5]);
     let b = hex_to_u32(&hex_colour[5..7]);
-    return [r, g, b]
+    return vec![r, g, b]
 }
 
 fn hex_to_u32(hex_string: &str) -> u32 {
@@ -27,10 +28,14 @@ fn hex_to_u32(hex_string: &str) -> u32 {
     return parsed_int;
 }
 
-fn rgb_to_hsl(rgb: [u32;3]) -> [u32;3] {
-    let [r,g,b] =rgb;
-    let min = cmp::min(r, cmp::min(g,b)) as f32;
-    let max = cmp::max(r, cmp::max(g,b)) as f32;
+#[wasm_bindgen]
+pub fn rgb_to_hsl(rgb: Vec<u32>) -> Vec<u32> {
+    if rgb.len() != 3 {
+        return vec![0,0,0]
+    }
+
+    let min = *rgb.iter().min().unwrap() as f32;
+    let max = *rgb.iter().max().unwrap() as f32;
     let delta  = max - min;
     let l = min + max/2.;
     
@@ -42,9 +47,9 @@ fn rgb_to_hsl(rgb: [u32;3]) -> [u32;3] {
 
     let mut h = 0.0;
     if delta > 0. {
-        let rf = r as f32;
-        let gf = g as f32;
-        let bf = b as f32;
+        let rf = rgb[0] as f32;
+        let gf = rgb[1] as f32;
+        let bf = rgb[2] as f32;
 
         if max == rf && max != gf  {
             h += (gf-bf)/delta
@@ -60,7 +65,7 @@ fn rgb_to_hsl(rgb: [u32;3]) -> [u32;3] {
         h /= 6.;
     }
 
-    return [(h * 255.) as u32, (s * 255.) as u32, (l * 255.) as u32 ];
+    return vec![(h * 255.) as u32, (s * 255.) as u32, (l * 255.) as u32 ];
 }
 
 #[wasm_bindgen]
